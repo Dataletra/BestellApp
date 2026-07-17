ac447f0305d60a65c2d3e6047648e808da01c5df
unknown <dataletra@gmail.com>
2026-07-17 15:22:29 +0200

    responsiveness adjustments, added rest of food items in db, indent fix in templateFunctions

--- scripts/db.js
+++ scripts/db.js
@@ -3,6 +3,12 @@ let foodItems = [
         category: "Burgers",
         items: [
             {
+                foodTitle: "Veggie mushroom black burger",
+                foodDescription: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+                foodPrice: "15,90",
+                foodImageFileName: "veggieMushroomBurger.jpg"
+            },
+            {
                 foodTitle: "All meat burger",
                 foodDescription: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
                 foodPrice: "15,90",
@@ -13,6 +19,12 @@ let foodItems = [
                 foodDescription: "Beef, Cheese, Tomatoes, Lettuce, Onion",
                 foodPrice: "14,90",
                 foodImageFileName: "redburger.jpg"
+            },
+            {
+                foodTitle: "Blg Chicken burger",
+                foodDescription: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
+                foodPrice: "15,90",
+                foodImageFileName: "chickenburger.jpg"
             }
         ]
     },
@@ -20,16 +32,28 @@ let foodItems = [
         category: "Pizzas",
         items: [
             {
-                foodTitle: "Pizza 1",
-                foodDescription: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-                foodPrice: "15,90",
-                foodImageFileName: "meatburger.jpg"
+                foodTitle: "Pizza Margherita",
+                foodDescription: "Tomato Sauce, Mozarella",
+                foodPrice: "11,90",
+                foodImageFileName: "pizzaMargherita.jpg"
             },
             {
-                foodTitle: "Pizza 2",
-                foodDescription: "Beef, Cheese, Tomatoes, Lettuce, Onion",
-                foodPrice: "14,90",
-                foodImageFileName: "redburger.jpg"
+                foodTitle: "Pizza Chorizo",
+                foodDescription: "Tomato slices, Mozzarella, Chorizo",
+                foodPrice: "13,90",
+                foodImageFileName: "pizzaChorizo.jpg"
+            },
+            {
+                foodTitle: "Funghi",
+                foodDescription: "Red onion, Olives, Button Mushrooms, Mozzarella",
+                foodPrice: "13,90",
+                foodImageFileName: "pizzaFunghi.jpg"
+            },
+            {
+                foodTitle: "Quattro Formaggi with Chicken",
+                foodDescription: "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+                foodPrice: "13,90",
+                foodImageFileName: "pizzaFormaggiChicken.jpg"
             }
         ]
     },
@@ -37,16 +61,28 @@ let foodItems = [
         category: "Salads",
         items: [
             {
-                foodTitle: "Salad 1",
-                foodDescription: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-                foodPrice: "15,90",
-                foodImageFileName: "meatburger.jpg"
+                foodTitle: "Warm beef arugula salad",
+                foodDescription: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+                foodPrice: "16,90",
+                foodImageFileName: "saladBeefArugula.jpg"
             },
             {
-                foodTitle: "Salad 2",
-                foodDescription: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+                foodTitle: "Mini green Salad",
+                foodDescription: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+                foodPrice: "7,90",
+                foodImageFileName: "saladMini.jpg"
+            },
+            {
+                foodTitle: "Green Salad with sea food",
+                foodDescription: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+                foodPrice: "16,90",
+                foodImageFileName: "saladSeafood.jpg"
+            },
+            {
+                foodTitle: "Vegan green salad with tofu",
+                foodDescription: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
                 foodPrice: "14,90",
-                foodImageFileName: "redburger.jpg"
+                foodImageFileName: "saladTofu.jpg"
             }
         ]
     }
