1fe5306a54c9a46f81e887807dff58313d801dd9
unknown <dataletra@gmail.com>
2026-07-23 15:16:30 +0200

    basket css, pricing and render functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,13 @@
-let basket = [];
-let sum = 0;
 const basketRef = document.getElementById("basket");
 const basketItemRef = document.getElementById("basket-item-container");
+const subtotalRef = document.getElementById("subtotal");
+const totalRef = document.getElementById("total");
+const buyButtonRef = document.getElementById("buy-now-button");
+
 
 //TODO
 // fix position sticky on basket
-// decide to have it render with div or modal
-// fix the basket item call not working
-//add amount to db
-
-//render through db and see if there is any amount bigger than 1, if yes, render that item
-// basket into indexhtml auslagern, dann rendern wie gewohnt, auch als div, anzeigen mit mobile machen durch classlistadd
+//anzeigen mit mobile machen durch classlistadd
 //permanent anzeigen in desktop view
 function renderStoreListing() {
     let foodSectionRef = "";
@@ -32,9 +29,6 @@ function renderStoreListing() {
     }
 }
 
-
-
-
 function addToBasket(categoryID, itemID) {
     foodItems[categoryID].items[itemID].amount++
     if (foodItems[categoryID].items[itemID].amount == 1) {
@@ -42,14 +36,17 @@ function addToBasket(categoryID, itemID) {
     } else {
         let basketItemAmountId = document.getElementById(`basket-item-amount-id` + categoryID + "_" + itemID);
         let basketItemPriceId = document.getElementById(`basket-item-price-id` + categoryID + "_" + itemID);
-        basketItemAmountId.innerHTML = /*html*/`Amount: ${foodItems[categoryID].items[itemID].amount}`;
-        basketItemPriceId.innerHTML = /*html*/`$${getPrice(categoryID, itemID)}`;
+        basketItemAmountId.innerHTML = /*html*/`${foodItems[categoryID].items[itemID].amount}x`;
+        basketItemPriceId.innerHTML = /*html*/`$${getPrice(categoryID, itemID).toFixed(2)}`;
     }
+    updateBasketPrice();
 }
 
 function getPrice(categoryID, itemID) {
     let amount = foodItems[categoryID].items[itemID].amount;
-    return amount * foodItems[categoryID].items[itemID].foodPrice;
+    let price = foodItems[categoryID].items[itemID].foodPrice;
+    let calculatedPrice = amount * price;
+    return calculatedPrice;
 }
 
 function renderBasketItem() {
@@ -58,16 +55,36 @@ function renderBasketItem() {
         for (let itemID = 0; itemID < foodItems[categoryID].items.length; itemID++) {
             if (foodItems[categoryID].items[itemID].amount > 0) {
                 basketItemRef.innerHTML += renderItemInBasket(categoryID, itemID);
-            } else console.log("asd");
+            }
         }
     }
+    updateBasketPrice();
 }
 
-function getSumBasket() {
-    for (let i = 0; i < basket.length; i++) {
-        console.log(basket);
-        sum += basket[i].foodPrice;
+function deleteFromBasket(categoryID, itemID) {
+    foodItems[categoryID].items[itemID].amount = 0;
+    renderBasketItem();
+}
+
+function subtractFromBasket(categoryID, itemID) {
+    foodItems[categoryID].items[itemID].amount--;
+    renderBasketItem();
+}
+
+
+function updateBasketPrice() {
+    let updatedPrice = 0;
+    subtotalRef.innerHTML = "";
+    totalRef.innerHTML = "";
+    for (let categoryID = 0; categoryID < foodItems.length; categoryID++) {
+        for (let itemID = 0; itemID < foodItems[categoryID].items.length; itemID++) {
+            if (foodItems[categoryID].items[itemID].amount > 0) {
+                updatedPrice += getPrice(categoryID, itemID);
+            }
+        }
     }
+    subtotalRef.innerHTML = `$${updatedPrice.toFixed(2)}`;
+    totalRef.innerHTML = `$${(updatedPrice + 5.99).toFixed(2)}`;
 }
 
 renderStoreListing();
--- scripts/templateFunctions.js
+++ scripts/templateFunctions.js
@@ -19,9 +19,36 @@ function renderFoodCard(categoryID, itemID) {
 function renderItemInBasket(categoryID, itemID) {
     return /*html*/`
             <div class="item">
-                <p>${foodItems[categoryID].items[itemID].foodTitle}</p>
-                <p id="basket-item-amount-id${categoryID}_${itemID}">Amount: ${foodItems[categoryID].items[itemID].amount}</p>
-                <p id="basket-item-price-id${categoryID}_${itemID}">$${foodItems[categoryID].items[itemID].foodPrice}</p>
+                <div class="item-top-container">
+                    
+                    <p>${foodItems[categoryID].items[itemID].foodTitle}</p>
+                    <button class="trash-button" id="basket-item-delete-id${categoryID}_${itemID}" 
+                    onclick="deleteFromBasket(${categoryID}, ${itemID})">
+                    <img src="./assets/media/trash-icon.png">
+                    </button>
+                </div>
+                <div class="item-bottom-container">
+                    <div class="item-bottom-button-container">
+                        <button class="trash-button" id="basket-item-delete-id${categoryID}_${itemID}" 
+                        onclick="subtractFromBasket(${categoryID}, ${itemID})">
+                        <p>-</p>
+                        </button>
+                        <p id="basket-item-amount-id${categoryID}_${itemID}">${foodItems[categoryID].items[itemID].amount}x</p>
+                        <button class="trash-button" id="basket-item-delete-id${categoryID}_${itemID}" 
+                        onclick="addToBasket(${categoryID}, ${itemID})">
+                        <p>+</p>
+                        </button>
+                    </div>
+                    <p id="basket-item-price-id${categoryID}_${itemID}">$${getPrice(categoryID, itemID)}</p>
+                </div>
             </div>
         `
+}
+
+function renderPriceSection() {
+    return /*html*/`
+        <div>
+            
+        </div>
+    `
 }
